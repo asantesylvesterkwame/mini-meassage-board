bfa40ef230bc3667976f62ebd08c188a7e956341
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-06 15:23:45 +0000

    added a readable realtime update functionality using momentjs package

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,33 @@
 var express = require("express");
 var router = express.Router();
+const moment = require("moment");
+
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date(),
+    added: moment(Date.now()),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date(),
+    added: moment(Date.now()),
   },
 ];
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index",{ title: "Mini Messageboard", messages: messages });
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
-
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.author,
+    user: req.body.messageText,
+    added: moment(Date.now()),
+  });
+  res.redirect("/");
+});
+console.log(messages);
 module.exports = router;
