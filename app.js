20b7fdc181eaf6bf6aa939f1cb1a114ca0985817
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-07 17:16:22 +0000

    linted my file

--- app.js
+++ app.js
@@ -1,45 +1,64 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-app.listen(4000, ()=>{
-  console.log("server is running");
-})
+app.use(function (err, req, res, next) {
+  // This is a simple example, you might want to do more complex error handling
+  res.status(500);
+  res.render("form", { errorMsg: "Nothing to submit, Try Again!" });
+});
 
+const dbConnect = () => {
+  mongoose
+    .connect("mongodb+srv://sly:sly@neural-feed.kjiocqi.mongodb.net/")
+    .then(() => {
+      console.log("Database is connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+app.listen(4000, () => {
+  dbConnect();
+  console.log("server is running");
+});
 
 module.exports = app;
