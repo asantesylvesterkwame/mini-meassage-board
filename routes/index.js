1581a0f9f2fa55bf6c5163cca636e7e82fc8d8c0
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-07 17:20:39 +0000

    implemented momentjs and also integrated the database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const moment = require("moment");
+const messageModel = require("../models/message.model");
 
 const messages = [
   {
@@ -15,19 +16,40 @@ const messages = [
   },
 ];
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Mini Messageboard", messages: messages });
+router.get("/", async function (req, res, next) {
+  const messageData = await messageModel.find();
+  const formattedMessages = messageData.map((message) => ({
+    ...message.toObject(),
+    added: moment(message.added), // Convert the string to a moment object
+  }));
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: formattedMessages,
+  });
+  next();
 });
 router.get("/new", function (req, res, next) {
   res.render("form");
 });
-router.post("/new", function (req, res, next) {
-  messages.push({
-    text: req.body.author,
-    user: req.body.messageText,
-    added: moment(Date.now()),
-  });
-  res.redirect("/");
+router.post("/new", async function (req, res, next) {
+  try {
+    if (req.body.messageText == "") {
+      if (req.body.author == "") {
+        return res.render("form", { errorMsg: "Nothing to send, Try again" });
+      }
+    } else {
+      await messageModel.create({
+        author: req.body.author,
+        textMessage: req.body.textMessage,
+        added: moment(),
+      });
+
+      res.redirect("/");
+    }
+    res.send("submitted succefully");
+    res.json({ messageInfo: messageModel });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
-console.log(messages);
 module.exports = router;
